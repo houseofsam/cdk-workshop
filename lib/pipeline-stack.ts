1af1044e76b324bca53e0da811dc47540d60258e
EC2 Default User <ec2-user@ip-172-31-19-49.ca-central-1.compute.internal>
2022-08-01 14:56:25 +0000

    Define empty pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,15 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
         
-        new codecommit.Repository(this, 'WorkshopRepo', {
+        const repo = new codecommit.Repository(this, 'WorkshopRepo', {
             repositoryName: "WorkshopRepo"
         });
         
-        //pipeline code
+        //basic pipeline declaration. This sets initial struture of pipeline
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+           pipelineName: 'WorkshopPipeline',
+           synth: new CodeBuildStep('SynthStep', {
+               input: CodePipelineSource.codeCommit(repo, 'master'),
+               installCommands: [
+                    'npm install -g aws-cdk'   
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npm cdk synth'
+                ]
+           })
+        });
     }
 }
